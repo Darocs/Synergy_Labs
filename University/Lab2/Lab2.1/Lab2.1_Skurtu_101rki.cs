f2eb2d42d13fca97b7b19e26f0af9b7014fa0c52
Darocs <Darocs@mail.ru>
2023-11-11 17:03:26 +0300

    Added Lab2.1 If you need to run this lab, comment on another one

---
+++ University/Lab2/Lab2.1/Lab2.1_Skurtu_101rki.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Lab2_1
+{
+    static void Main()
+    {
+        try
+        {
+            /// <summary>
+            /// Укажите путь к вашему текстовому файлу
+            /// </summary>
+            string filePath = "путь_к_вашему_файлу.txt";
+
+            /// Загрузка предложений из файла ///
+            string[] sentences = LoadSentences(filePath);
+
+            /// Проверка, достаточно ли предложений в файле ///
+            if (sentences.Length >= 3)
+            {
+                /// Выбор первых трех предложений, их обратное упорядочивание и вывод ///
+                var firstThreeSentences = sentences.Take(3).Reverse();
+
+                Console.WriteLine("Первые три предложения в обратном порядке:");
+
+                /// Вывод предложений ///
+                foreach (var sentence in firstThreeSentences)
+                {
+                    Console.WriteLine(sentence);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файл содержит менее трех предложений. Невозможно выполнить задачу.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+
+    /// <summary>
+    /// Загружает предложения из текстового файла.
+    /// </summary>
+    /// <param name="filePath">Путь к текстовому файлу.</param>
+    /// <returns>Массив строк, представляющих предложения.</returns>
+    static string[] LoadSentences(string filePath)
+    {
+        /// Чтение строк из файла ///
+        string[] lines = File.ReadAllLines(filePath);
+
+        /// Разделение строк на предложения ///
+        char[] separators = { '.', '!', '?' };
+        var sentences = lines.SelectMany(line => line.Split(separators, StringSplitOptions.RemoveEmptyEntries));
+
+        return sentences.Select(sentence => sentence.Trim()).ToArray();
+    }
+}
